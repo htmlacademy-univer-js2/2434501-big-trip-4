9eaa17f486f11ac8b85678fe9e4e5cd859df8400
CreatFun <129187437+CreatFun@users.noreply.github.com>
2024-04-16 00:09:40 +0500

    фикс

--- src/view/event-editing-form.js
+++ src/view/event-editing-form.js
@@ -77,7 +77,7 @@ function createEventEditingFormTemplate(point) {
         <label class="event__label  event__type-output" for="event-destination-1">
           ${type}
         </label>
-        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value=${destination} list="destination-list-1">
+        <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value=${'destination'} list="destination-list-1">
         <datalist id="destination-list-1">
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
